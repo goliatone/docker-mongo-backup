73b20f3665581c75acb42f3e777d861b63d4a750
Gabriel Ruiz <gabe@ggruiz.me>
2015-06-26 11:47:58 +0200

    fixed

--- cronstart.sh
+++ cronstart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 touch tmpcron
-echo "0 2 * * * ./backup.sh" >> tmpcron
-echo "0 5 * * * ./remove.sh" >> tmpcron
-echo "* * * * * ./test.sh" >> tmpcron
+echo "0 2 * * * /backup.sh" >> tmpcron
+echo "0 5 * * * /remove.sh" >> tmpcron
 crontab tmpcron
 rm tmpcron
