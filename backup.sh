f61fe665ce24fed5cc2841c3dd48e02f24913d57
goliatone <burgosemi@gmail.com>
2016-06-12 20:41:50 -0400

    refactoring

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-mongodump -h mongo
+mongodump -h $MONGO_HOST -p $MONGO_PORT
+# mongodump -h mongo
 tar -zcvf "/mongobackup/backup-$TIMESTAMP.tar.gz" dump/
 rm -rf dump/
