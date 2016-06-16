02c3508635a715c0330079fe9b9f32be2550d7aa
goliatone <burgosemi@gmail.com>
2016-06-16 18:09:44 -0400

    backup: fix test

--- backup.sh
+++ backup.sh
@@ -18,16 +18,16 @@ tar -zcvf $OUT dump/
 rm -rf dump/
 
 #Respect the spaces between brackes and statement!!! MOFO
-if [ "$S3_BUCKET" = false ] || [ "$AWS_ACCESS_KEY_ID" = false ] || [ "$AWS_SECRET_ACCESS_KEY" = false ];
+if [[ "$S3_BUCKET" ]] || [[ "$AWS_ACCESS_KEY_ID" ]] || [[ "$AWS_SECRET_ACCESS_KEY" ]];
 then
+    echo "$(get_date) [3/3] Uploading to S3..."
+    /usr/local/bin/aws s3 cp $OUT s3://$S3_BUCKET/
+else
     echo "$(get_date) [3/3] Skeeping upload to S3."
     echo "$(get_date) [3/3] If this is an error, ensure all AWS environment variables are set."
     echo "$(get_date) [3/3] S3_BUCKET=$S3_BUCKET"
     echo "$(get_date) [3/3] AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
     echo "$(get_date) [3/3] AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
-else
-    echo "$(get_date) [3/3] Uploading to S3..."
-    /usr/local/bin/aws s3 cp $OUT s3://$S3_BUCKET/
 fi
 
 echo "$(get_date) Mongo backup complted"
