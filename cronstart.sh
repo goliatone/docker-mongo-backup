2f725fac9de395024573cb103f39ba14d710da6c
goliatone <burgosemi@gmail.com>
2016-07-11 14:53:59 -0400

    Update cronstart to ensure empty env vars do not break the script

--- cronstart.sh
+++ cronstart.sh
@@ -4,15 +4,16 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-0 2 * * *}
 CRON_CLEANUP=${CRON_CLEANUP:-0 5 1 * *}
 
 CRON_ENVIRONMENT="
-MONGO_HOST=${MONGO_HOST:-mongo}
-MONGO_PORT=${MONGO_PORT:-27017}
-S3_BUCKET=${S3_BUCKET}
-AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+S3_BUCKET=${S3_BUCKET};
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID};
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY};
+MONGO_PORT=${MONGO_PORT:-27017};
+MONGO_HOST=${MONGO_HOST:-mongo};
 "
 
 CRON_COMMAND="/backup.sh 1>/var/log/backup_script.log 2>&1"
 
+echo "VERSION: 0.0.1"
 echo "Start mongobackup script..."
 echo
 echo "CRON_SCHEDULE"
@@ -25,7 +26,7 @@ touch tmpcron
 echo "$CRON_ENVIRONMENT$CRON_SCHEDULE $CRON_COMMAND" >> tmpcron
 echo "$CRON_CLEANUP /remove.sh" >> tmpcron
 crontab tmpcron
-rm tmpcron
+# rm tmpcron
 
 cron start -f
-tail -f /var/log/backup_script.log
+# tail -f /var/log/backup_script.log
